0413d7313701a6adb8520b40d709614616070f48
Saeed99Madi <saed.zooro@gmail.com>
2022-12-06 22:17:28 +0200

    edit dashboard context interface

--- client/src/interfaces/IDashboardContext.ts
+++ client/src/interfaces/IDashboardContext.ts
@@ -1,8 +1,8 @@
 import { Dispatch, SetStateAction } from 'react';
 
 interface IDashboardContext {
-  searchFilterCategory: number;
-  setSearchFilterCategory: Dispatch<SetStateAction<number>>;
+  searchFilterCategory: string;
+  setSearchFilterCategory: Dispatch<SetStateAction<string>>;
   productSearch: string;
   setProductSearch: Dispatch<SetStateAction<string>>;
   openSideBar: boolean;
