51cebdfd900f0a27b1c8946dc8e38d87ec40bed5
Saeed99Madi <saed.zooro@gmail.com>
2022-12-11 18:25:40 +0200

    edit product controller related to #37

--- src/schemes/product.ts
+++ src/schemes/product.ts
@@ -4,9 +4,6 @@ const productValid = (data: object) => {
   const schema = Joi.object({
     title: Joi.string().min(4).max(50).required(),
     description: Joi.string().min(50).max(5000).required(),
-    cover: Joi.string().min(50).max(256).required(),
-    price: Joi.number().min(1).max(9999999).required(),
-    salePrice: Joi.number().min(1).max(9999999).required(),
   });
   return schema.validateAsync(data);
 };
