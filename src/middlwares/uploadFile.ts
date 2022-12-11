7b69479d0596fa09d981d544bcc15175917281a8
Saeed99Madi <saed.zooro@gmail.com>
2022-12-11 18:22:47 +0200

    edit multer configration in router and middleware related to #37

--- src/middlwares/uploadFile.ts
+++ src/middlwares/uploadFile.ts
@@ -4,9 +4,11 @@ import path from 'path';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, '../imeges');
+    cb(null, path.join(__dirname, '../images/'));
   },
   filename: (req, file, cb) => {
+    console.log(path.extname(file.originalname));
+
     console.log(file);
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
     cb(null, uniqueSuffix + path.extname(file.originalname));
