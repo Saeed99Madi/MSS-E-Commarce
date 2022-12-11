186582dafb0452713d03962ac4b2af2940ebe964
Saeed99Madi <saed.zooro@gmail.com>
2022-12-11 19:51:19 +0200

    add product component related to #37

--- client/src/components/dashboard/Products/AddProduct/index.tsx
+++ client/src/components/dashboard/Products/AddProduct/index.tsx
@@ -1,29 +1,35 @@
 import * as React from 'react';
 import uuid from 'react-uuid';
 
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
+import {
+  Box,
+  Button,
+  Dialog,
+  Checkbox,
+  Toolbar,
+  IconButton,
+  Typography,
+  Divider,
+  AppBar,
+  Slide,
+} from '@mui/material';
 
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import AppBar from '@mui/material/AppBar';
-
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
-import NoteAddIcon from '@mui/icons-material/NoteAdd';
-
-import Slide from '@mui/material/Slide';
 import { TransitionProps } from '@mui/material/transitions';
-import { TextField, Select, MenuItem, SelectChangeEvent } from '@mui/material';
-
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import AddIcon from '@mui/icons-material/Add';
 import TextInput from './TextInput';
 
-import { arrowIcon } from '../../components.styled';
+import { DescriptionInput, InputsList } from './components.styled';
+import ApiServices from '../../../../servises/ApiService';
+import { IProduct } from '../../../../interfaces/IProduct';
 
+import { SelectCategories } from './SelectCategories';
+import { UploadProductFiles } from './UploadProductFiles';
+import { AttriputeReducer } from '../../../../helpers/AttriputeReducer';
+
+import { AttriputeForms } from './AttriputeForms';
+
+ApiServices.init();
 const Transition = React.forwardRef(
   (
     props: TransitionProps & {
@@ -35,56 +41,34 @@ const Transition = React.forwardRef(
     return <Slide direction="up" ref={ref} {...props} />;
   },
 );
-type Product = {
-  title: string;
-  cover: string;
-  description: string;
-  CategoryId: string;
-  catalog: string;
-};
-type Attripute = {
-  id: string;
-  title: string;
-  description: string;
-};
+
 const initialState = [{ id: uuid(), title: '', description: '' }];
-
-const reducer = (state: Attripute[], action: any) => {
-  let objindex: number;
-  let oldAttr: Attripute;
-
-  switch (action.type) {
-    case 'add':
-      return [...state, { ...action.attripute, id: uuid() }];
-    case 'edit':
-      objindex = state.findIndex(obj => obj.id === action.id);
-      oldAttr = { ...state[objindex] };
-      oldAttr.title = action.newAttr.title;
-      oldAttr.description = action.newAttr.description;
-      // eslint-disable-next-line no-param-reassign
-      state[objindex] = { ...oldAttr };
-      return [...state];
-    case 'remove':
-      return state.filter(ele => ele.id !== action.id);
-    default:
-      throw new Error('error in Attriputes Dispatcher');
-  }
+type Props = {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
-const AddProduct = () => {
-  const [open, setOpen] = React.useState(false);
-  const [category, setategory] = React.useState('');
-  const [newProduct, setNewProduct] = React.useState<Product>({
+const AddProduct = (props: Props) => {
+  const { open, setOpen } = props;
+  const [category, setCategory] = React.useState('');
+  const [newProduct, setNewProduct] = React.useState<IProduct>({
     title: '',
-    cover: '',
     description: '',
     CategoryId: '',
-    catalog: '',
+    active: false,
+    gallery: [],
   });
+  const [isChecked, setIsChecked] = React.useState(false);
 
   const [attriputes, attriputesDispatch] = React.useReducer(
-    reducer,
+    AttriputeReducer,
     initialState,
   );
+
+  const handleIsChecked = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { checked } = e.target;
+    setIsChecked(checked);
+    setNewProduct(prev => ({ ...prev, active: checked }));
+  };
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -95,36 +79,55 @@ const AddProduct = () => {
   };
   const handleProductChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
-    console.log(newProduct);
-
     setNewProduct(prev => ({ ...prev, [name]: value }));
   };
-  const handlCategory = (event: SelectChangeEvent) => {
-    setategory(event.target.value);
-    setNewProduct(prev => ({ ...prev, CategoryId: event.target.value }));
-  };
-  const handleNewAttrChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(attriputes);
-    console.log(e.target.id);
 
-    attriputesDispatch({
-      type: 'edit',
-      newAttr: { id: e.target.id, description: '', title: e.target.value },
-      id: e.target.id,
+  const handleSaveProduct = () => {
+    const data = new FormData();
+    if (!newProduct.cover || !newProduct.gallery || !newProduct.catalog) {
+      return;
+    }
+    data.append('cover', newProduct.cover);
+    data.append('catalog', newProduct.catalog);
+    const productGallery = [...newProduct.gallery];
+    productGallery.forEach(image => {
+      data.append('gallery', image, image.name);
     });
+
+    data.append('title', newProduct.title);
+    data.append('CategoryId', newProduct.CategoryId);
+    data.append('description', newProduct.description);
+    const AttriputesStr = JSON.stringify(attriputes);
+    data.append('attriputes', AttriputesStr);
+
+    ApiServices.post('products', data);
   };
 
   return (
-    <div>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Open full-screen dialog
+    <Box>
+      <Button
+        sx={{
+          width: '15rem',
+          height: 'auto',
+          background:
+            'linear-gradient(125.86deg, rgba(255, 255, 255, 0.18) -267.85%, rgba(255, 255, 255, 0) 138.29%)',
+          backdropFilter: 'blur(5.73932px)',
+          borderRadius: '12px',
+          color: '#FFFFFF',
+        }}
+        variant="outlined"
+        onClick={handleClickOpen}
+        startIcon={<AddIcon />}
+      >
+        Add Product
       </Button>
+
       <Dialog
         fullScreen
         open={open}
         onClose={handleClose}
         TransitionComponent={Transition}
-        sx={{ minHeight: '100%', top: '4rem', background: '#141417' }}
+        sx={{ height: 'auto', top: '4rem', background: '#141417' }}
       >
         <AppBar
           sx={{
@@ -142,24 +145,25 @@ const AddProduct = () => {
               <CloseIcon />
             </IconButton>
             <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-              Sound
+              Add Product
             </Typography>
-            <Button autoFocus color="inherit" onClick={handleClose}>
+            <Button
+              autoFocus
+              color="inherit"
+              onClick={() => {
+                handleClose();
+                handleSaveProduct();
+              }}
+            >
               save
             </Button>
           </Toolbar>
         </AppBar>
 
-        <List
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            flexDirection: 'column',
-            background: '#141417',
-            gap: '1.5rem',
-            minHeight: '100%',
-          }}
-        >
+        <InputsList>
+          <Typography sx={{ marginRight: 'auto', color: '#FFFFFF' }}>
+            Add New Product
+          </Typography>
           <TextInput
             handleProductChange={handleProductChange}
             name="title"
@@ -167,18 +171,8 @@ const AddProduct = () => {
             label="Product Title"
           />
 
-          <TextField
-            sx={{
-              borderRadius: '0.5rem',
-              input: {
-                color: '#FFFFFF',
-              },
-              label: { color: '#b1a9a9' },
-              width: '50%',
-
-              background:
-                'linear-gradient(130.79deg, rgba(255, 255, 255, 0.08) -37.1%, rgba(255, 255, 255, 0) 134.47%)',
-            }}
+          <DescriptionInput
+            key="Product Description"
             name="description"
             onChange={handleProductChange}
             value={newProduct.description}
@@ -188,111 +182,28 @@ const AddProduct = () => {
             rows={4}
           />
 
-          <Select
-            sx={{
-              borderRadius: '0.5rem',
-              background:
-                'linear-gradient(130.79deg, rgba(255, 255, 255, 0.08) -37.1%, rgba(255, 255, 255, 0) 134.47%)',
-              width: '50%',
-              color: '#FFFFFF',
-            }}
-            IconComponent={arrowIcon}
-            name="CategoryId"
-            MenuProps={{
-              PaperProps: {
-                style: { width: '250px', height: '50%', overflowY: 'scroll' },
-              },
-            }}
-            value={category}
-            onChange={handlCategory}
-            displayEmpty
-            inputProps={{ 'aria-label': 'Without label' }}
-          >
-            <MenuItem value="">
-              <em>Category</em>
-            </MenuItem>
-            <MenuItem value="10">Ten</MenuItem>
-            <MenuItem value="20">Twenty</MenuItem>
-            <MenuItem value="30">Thirty</MenuItem>
-            <MenuItem value="10">Ten</MenuItem>
-            <MenuItem value="20">Twenty</MenuItem>
-            <MenuItem value="30">Thirty</MenuItem>
-            <MenuItem value="10">Ten</MenuItem>
-            <MenuItem value="20">Twenty</MenuItem>
-            <MenuItem value="30">Thirty</MenuItem>
-            <MenuItem value="20">Twenty</MenuItem>
-            <MenuItem value="30">Thirty</MenuItem>
-            <MenuItem value="10">Ten</MenuItem>
-            <MenuItem value="20">Twenty</MenuItem>
-            <MenuItem value="30">Thirty</MenuItem>
-          </Select>
+          <SelectCategories
+            setCategory={setCategory}
+            category={category}
+            setNewProduct={setNewProduct}
+          />
 
-          <Button
-            sx={{
-              borderRadius: '0.5rem',
-              padding: '1rem 0',
-              width: '50%',
-              background:
-                'linear-gradient(130.79deg, rgba(255, 255, 255, 0.08) -37.1%, rgba(255, 255, 255, 0) 134.47%)',
-            }}
-            variant="contained"
-            component="label"
-          >
-            <CloudUploadIcon sx={{ marginRight: '1rem' }} />
-            Product Cover
-            <input hidden accept="image/*" type="file" />
-          </Button>
-
-          <Button
-            sx={{
-              borderRadius: '0.5rem',
-              padding: '1rem 0',
-              width: '50%',
-              background:
-                'linear-gradient(130.79deg, rgba(255, 255, 255, 0.08) -37.1%, rgba(255, 255, 255, 0) 134.47%)',
-            }}
-            variant="contained"
-            component="label"
-          >
-            <CloudUploadIcon sx={{ marginRight: '1rem' }} />
-            Product Gallery
-            <input hidden accept="image/*" multiple type="file" />
-          </Button>
+          <UploadProductFiles setNewProduct={setNewProduct} />
+          <AttriputeForms
+            attriputes={attriputes}
+            attriputesDispatch={attriputesDispatch}
+          />
           <Divider sx={{ height: '1px', width: '50%' }} color="#FFFFFF" />
-          <Typography>Other Fields</Typography>
-
-          {attriputes.map(attripute => {
-            return (
-              <TextField
-                key={attripute.id}
-                sx={{
-                  borderRadius: '0.5rem',
-                  input: { color: '#FFFFFF' },
-                  label: { color: '#b1a9a9', marginLeft: '1rem' },
-                  width: '50%',
-                  background:
-                    'linear-gradient(130.79deg, rgba(255, 255, 255, 0.08) -37.1%, rgba(255, 255, 255, 0) 134.47%)',
-                }}
-                onChange={handleNewAttrChange}
-                value={attripute.title}
-                id={attripute.id}
-                label="Attripute title"
-                variant="standard"
-              />
-            );
-          })}
-          <Divider sx={{ height: '1px', width: '50%' }} color="#FFFFFF" />
-          <Button
-            variant="outlined"
-            sx={{ color: '#FFFFFF' }}
-            startIcon={<NoteAddIcon />}
-          >
-            add New Field
-          </Button>
-          <Divider sx={{ height: '1px', width: '50%' }} color="#FFFFFF" />
-        </List>
+          <Checkbox
+            onChange={handleIsChecked}
+            defaultChecked
+            name="active"
+            value={isChecked}
+            sx={{ '& .MuiSvgIcon-root': { fontSize: 28 } }}
+          />
+        </InputsList>
       </Dialog>
-    </div>
+    </Box>
   );
 };
 export default AddProduct;
