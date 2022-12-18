c16b922b15414caca036ac51fae6b8e056a1af8d
Saeed99Madi <saed.zooro@gmail.com>
2022-12-18 16:17:26 +0200

    create Settings Router related to #54

---
+++ src/routes/settings.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+
+import { SettingsController } from '../controllers/';
+import errorWrapper from '../helpers/errorHandler/errorWrapper';
+import upload from '../middlwares/uploadFile';
+
+const router = Router();
+
+const SettingsFields = [
+  { name: 'logo', maxCount: 1 },
+  { name: 'logo2', maxCount: 1 },
+];
+router.put(
+  '/settings/update',
+  upload.fields(SettingsFields),
+  errorWrapper(SettingsController.update),
+);
+
+export default router;
