c655d30f4c168c04b767ba72ab5462c88c049d81
Saeed99Madi <saed.zooro@gmail.com>
2022-12-18 16:22:06 +0200

    integration chenges debend to adding settings Router related to #54

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,10 +4,12 @@ import authRouter from './auth';
 import productRouter from './products';
 import categoriesRouter from './categories';
 import servicesRouter from './services';
+import settingsRouter from './settings';
 
 const router = Router();
 router.use(authRouter);
 router.use(productRouter);
 router.use(categoriesRouter);
 router.use(servicesRouter);
+router.use(settingsRouter);
 export default router;
