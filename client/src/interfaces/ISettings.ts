344c61b8b54bd1607125ac4788e282a709335024
Saeed99Madi <saed.zooro@gmail.com>
2022-12-18 16:18:26 +0200

    create Client Side Settings Interface related to #54

---
+++ client/src/interfaces/ISettings.ts
@@ -0,0 +1,16 @@
+export interface ISettings {
+  id?: number;
+  email: string;
+  phone: string;
+  phone2: string;
+  address: string;
+  facebook: string;
+  instagram: string;
+  linkedIn: string;
+  youtupe: string;
+  logo?: string;
+  logo2?: string;
+  bio: string;
+  logoFile?: File;
+  logo2File?: File;
+}
