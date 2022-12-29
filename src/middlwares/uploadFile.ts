2970cb93b3da66075f461542f497693ced657e59
Saeed99Madi <saed.zooro@gmail.com>
2022-12-29 16:31:14 +0200

    create daynamic FileUploader related to #74

--- src/middlwares/uploadFile.ts
+++ src/middlwares/uploadFile.ts
@@ -2,17 +2,20 @@
 import multer from 'multer';
 import path from 'path';
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '../images/products'));
-  },
-  filename: (req, file, cb) => {
-    console.log(path.extname(file.originalname));
+const storageUpload = (pathImage: string): multer.Multer => {
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.join(__dirname, `../images/${pathImage}`));
+    },
+    filename: (req, file, cb) => {
+      console.log(path.extname(file.originalname));
 
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + path.extname(file.originalname));
-  },
-});
-const upload = multer({ storage });
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+      cb(null, uniqueSuffix + path.extname(file.originalname));
+    },
+  });
+  const upload = multer({ storage, limits: { fileSize: 1048576 } });
+  return upload;
+};
 
-export default upload;
+export default storageUpload;
