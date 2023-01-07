7138f0143e9f8375060cd3718a63e71202cceddb
Saeed99Madi <saed.zooro@gmail.com>
2023-01-08 01:54:44 +0200

    create call to action components related to #94

---
+++ client/components/Header/Actions/ActionButtons/index.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable no-console */
+import useTranslation from 'next-translate/useTranslation';
+import NativeSelect from '@mui/material/NativeSelect';
+import setLanguage from 'next-translate/setLanguage';
+import { AuthButtonsBox, LoginButton } from '../../components.styled';
+
+export const AuthButtons = () => {
+  const { t } = useTranslation('home');
+  const handleLanguageClick = (event: any) => {
+    console.log(event.target.value);
+    const lang = event.target.value;
+    setLanguage(lang);
+  };
+  return (
+    <AuthButtonsBox>
+      <NativeSelect
+        disableUnderline
+        sx={{ background: '#FFFFFF' }}
+        defaultValue={10}
+        onChange={handleLanguageClick}
+        inputProps={{
+          name: 'Language',
+        }}
+      >
+        <option value="en">En</option>
+        <option value="fr">Fr</option>
+      </NativeSelect>
+      <LoginButton
+        onClick={(event: any) => {
+          console.log(event);
+        }}
+        variant="outlined"
+      >
+        {t('Check Products')}
+      </LoginButton>
+    </AuthButtonsBox>
+  );
+};
