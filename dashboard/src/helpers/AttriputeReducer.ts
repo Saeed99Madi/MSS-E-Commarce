088022df6188d4cdc1548651ce67bc85b1da1149
Saeed99Madi <saed.zooro@gmail.com>
2023-01-23 16:17:16 +0200

    create more cases for edit Products related to #134

--- dashboard/src/helpers/AttriputeReducer.ts
+++ dashboard/src/helpers/AttriputeReducer.ts
@@ -4,12 +4,21 @@ type Attripute = {
   id: string;
   title: string;
   description: string;
+  productID?: number;
+  createdAt?: string;
+  updatedAt?: string;
 };
 
 export const AttriputeReducer = (state: Array<Attripute>, action: any) => {
   switch (action.type) {
     case 'add':
       return [...state, { ...action.attripute, id: uuid() }];
+    case 'removeAll':
+      return [];
+    case 'addAttriputes':
+      console.log(action.attriputes, 'her we go');
+
+      return [...state, ...action.attriputes];
     case 'edit':
       // eslint-disable-next-line no-param-reassign
       state[action.index] = { ...action.newAttr };
