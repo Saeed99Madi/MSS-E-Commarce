ccc0434e8e1d2a03a5e1f214a5b5c89974cb4d54
Mahmoud Hammad <67611935+mahmoudhammad309@users.noreply.github.com>
2023-01-31 18:26:35 +0200

    Revert "130 contact page"

---
+++ client/components/Contact/ContactByWhatsApp.tsx
@@ -0,0 +1,77 @@
+import CloseIcon from '@mui/icons-material/Close';
+import {
+  ContactWhatsAppSection,
+  WhatsAppWrapper,
+  ImageWrapper,
+  ImageIcon,
+  Spot,
+  CustomTypography,
+  SubmitBtn,
+  HeaderTypography,
+  WhatsAppSection,
+  LogoWrapper,
+  WhatsAppSectionWrapper,
+} from './components.styled';
+
+type Props = {
+  openWhatsApp: boolean;
+  openForm: boolean;
+  seOpenWhatsApp: Function;
+  SetOpenForm: Function;
+};
+
+const ContactByWhatsApp = ({
+  openWhatsApp,
+  seOpenWhatsApp,
+  SetOpenForm,
+  openForm,
+}: Props) => {
+  return (
+    <ContactWhatsAppSection
+      onMouseEnter={() => {
+        SetOpenForm(false);
+      }}
+      openWhatsApp
+      openForm={openForm}
+    >
+      <WhatsAppWrapper openWhatsApp={openWhatsApp}>
+        <div>
+          <ImageWrapper whatsapp>
+            <Spot hoverForm={openForm} whatsapp />
+            <ImageIcon src="/assets/contact-whatsapp.png" alt="contact" />
+          </ImageWrapper>
+          <CustomTypography>Contact Us</CustomTypography>
+          <HeaderTypography whatsapp>
+            Contact By <span>WhatsApp</span>
+          </HeaderTypography>
+        </div>
+        {!openWhatsApp && (
+          <SubmitBtn
+            whatsapp
+            onMouseEnter={() => {
+              seOpenWhatsApp(true);
+            }}
+          >
+            Chat with us
+          </SubmitBtn>
+        )}
+      </WhatsAppWrapper>
+      <WhatsAppSection openWhatsApp={openWhatsApp}>
+        <CloseIcon
+          sx={{ color: '#fff', alignSelf: 'flex-end', cursor: 'pointer' }}
+          onClick={() => {
+            seOpenWhatsApp(false);
+          }}
+        />
+        <WhatsAppSectionWrapper>
+          <LogoWrapper>
+            <ImageIcon src="/assets/logo.png" alt="contact" />
+          </LogoWrapper>
+          {openWhatsApp && <SubmitBtn whatsapp>Chat with us</SubmitBtn>}
+        </WhatsAppSectionWrapper>
+      </WhatsAppSection>
+    </ContactWhatsAppSection>
+  );
+};
+
+export default ContactByWhatsApp;
