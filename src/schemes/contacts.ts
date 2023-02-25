dd4e018b891cd6adb108d340301f752d8d52dcc0
Saeed99Madi <saed.zooro@gmail.com>
2023-02-25 13:50:26 +0200

    prepare contacts apis to be real to clients related to #173

--- src/schemes/contacts.ts
+++ src/schemes/contacts.ts
@@ -3,8 +3,9 @@ import * as Joi from 'joi';
 const contentSchema = (data: object) => {
   const schema = Joi.object({
     name: Joi.string().min(4).max(50).required(),
+    mobile: Joi.string().min(4).max(50).required(),
     email: Joi.string().email({ minDomainSegments: 2 }).required(),
-    content: Joi.string().min(50).max(5000).required(),
+    content: Joi.string().min(8).max(5000).required(),
   });
 
   return schema.validateAsync(data);
